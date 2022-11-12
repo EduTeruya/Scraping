23f4e3b3a99860efea9a2190bf147a20b44219a3
EduTeruya <eduardo.teruya.camt@gmail.com>
2022-11-12 11:46:06 -0500

    scrap5

--- Scrap.py
+++ Scrap.py
@@ -5,11 +5,13 @@ import time
 
 driver = webdriver.Chrome('C:\Github\Scraping\chromedriver.exe')
 driver.get('https://www.tiendaclaro.pe/personas/catalogo-celulares/postpago/portabilidad/si?initialMap=c,c,c&initialQuery=personas/catalogo-celulares/postpago/portabilidad/si&map=category-1,category-2,category-3,modalidad,plan-destacado&order=&query=/personas/catalogo-celulares/postpago/portabilidad/si&searchState&utm_campaign=performance_tienda&utm_source=google&utm_medium=search&utm_content=generico-postpago_serch&utm_term=postpago-portabilidad&cu=cp1668118049&gclid=Cj0KCQiAgribBhDkARIsAASA5bumfxx1s_VzyO_me9RXf8LtNldpKXCLTkSKpEJjOjt08i4zqCU2k-EaAuAKEALw_wcB')
-time.sleep(15)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(30)
 
 models = driver.find_elements_by_class_name('claroperupoc-claro-general-apps-0-x-product_name_content')
 prices = driver.find_elements_by_class_name("claroperupoc-claro-general-apps-0-x-product_price_container")
-
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(30)
 
 modelos = []
 for model in models:
@@ -21,8 +23,8 @@ for price in prices:
 
 
 # Pandas
-modelos_df = pd.DataFrame({'modelos':Modelos})
-precios_df = pd.DataFrame({'precios':Precios})
+modelos_df = pd.DataFrame({'Modelos':modelos})
+precios_df = pd.DataFrame({'Precios':precios})
 
 # unir los 2 frames en 1
 frames = [modelos_df, precios_df]
