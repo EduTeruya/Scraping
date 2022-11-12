cdca5de357ceda4af8b50c210087920cc1cfb780
EduTeruya <eduardo.teruya.camt@gmail.com>
2022-11-12 11:32:30 -0500

    scrap3

--- Scrap.py
+++ Scrap.py
@@ -1,24 +1,39 @@
-import time
-
 import selenium
 from selenium import webdriver
+import pandas as pd
+import time
 
 driver = webdriver.Chrome('C:\Github\Scraping\chromedriver.exe')
 driver.get('https://www.tiendaclaro.pe/personas/catalogo-celulares/postpago/portabilidad/si?initialMap=c,c,c&initialQuery=personas/catalogo-celulares/postpago/portabilidad/si&map=category-1,category-2,category-3,modalidad,plan-destacado&order=&query=/personas/catalogo-celulares/postpago/portabilidad/si&searchState&utm_campaign=performance_tienda&utm_source=google&utm_medium=search&utm_content=generico-postpago_serch&utm_term=postpago-portabilidad&cu=cp1668118049&gclid=Cj0KCQiAgribBhDkARIsAASA5bumfxx1s_VzyO_me9RXf8LtNldpKXCLTkSKpEJjOjt08i4zqCU2k-EaAuAKEALw_wcB')
-time.sleep(10)
+time.sleep(15)
 
 models = driver.find_elements_by_class_name('claroperupoc-claro-general-apps-0-x-product_name_content')
-prices = driver.find_elements_by_class_name('claroperupoc-claro-general-apps-0-x-product_price_value')
+prices = driver.find_elements_by_class_name("claroperupoc-claro-general-apps-0-x-product_price_container")
 
 
-
-for price in prices:
-    print((price).text)
-
+modelos = []
 for model in models:
-    print((model).text)
+    modelos.append(model.text)
 
-'''for price in prices:
-    for model in models:
-        print((model, price).text)
-'''
+precios = []
+for price in prices:
+    precios.append(price.text)
+
+
+# Pandas
+modelos_df = pd.DataFrame({'modelos':modelos})
+precios_df = pd.DataFrame({'precios':precios})
+
+# unir los 2 frames en 1
+frames = [modelos_df, precios_df]
+result = pd.concat((frames), axis=1, join='inner')
+print(result)
+
+
+
+# for price in prices:
+#     for model in models:
+#         print((model).text, (price).text)
+
+
+        
